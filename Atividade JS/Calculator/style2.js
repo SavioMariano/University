3dd95220ec82992f7b3e31e219f46b9cd14e7635
Savio <78896606+SavioMariano@users.noreply.github.com>
2025-10-19 10:34:28 -0300

    Add files via upload

---
+++ Atividade JS/Calculator/style2.js
@@ -0,0 +1,52 @@
+clear.addEventListener("click", function(){
+    campo.value= "";
+});
+btn1.addEventListener("click", function() {
+    campo.value += btn1.value;
+});
+btn2.addEventListener("click", function() {
+    campo.value += btn2.value;
+});
+btn3.addEventListener("click", function() {
+    campo.value += btn3.value;
+});
+btn4.addEventListener("click", function() { //somar
+    campo.value += btn4.value;
+
+});
+btn5.addEventListener("click", function() {
+    campo.value += btn5.value;
+});
+btn6.addEventListener("click", function() {
+    campo.value += btn6.value;
+})
+;btn7.addEventListener("click", function() {
+    campo.value += btn7.value;
+});
+btn8.addEventListener("click", function() { //subtrair
+    campo.value += btn8.value;
+})
+;btn9.addEventListener("click", function() {
+    campo.value += btn9.value;
+})
+btn10.addEventListener("click", function() {
+    campo.value += btn10.value;
+});
+btn11.addEventListener("click", function() {
+    campo.value += btn11.value;
+});
+btn12.addEventListener("click", function() { //multiplicar
+    campo.value += btn12.value;
+});
+btn13.addEventListener("click", function() {
+    campo.value += btn13.value;
+});
+btn14.addEventListener("click", function() {
+    campo.value += btn14.value;
+});
+btn15.addEventListener("click", function() { // igual
+    campo.value = eval(campo.value);
+}); 
+btn16.addEventListener("click", function() { //dividir
+    campo.value += btn16.value;
+});
